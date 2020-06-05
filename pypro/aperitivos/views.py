5fc28aeae130b155a621eab7559c6ceb408b8f8f
Alyson Girao <alysongirao@gmail.com>
2020-06-05 18:52:06 -0300

    Adicionada configuracao do postgres

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,29 +1,12 @@
-from django.shortcuts import render
-from django.urls import reverse
-
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
-
-videos = [
-    Video('motivacao', 'Video Aperitivo: Motivação', 423820751),
-    Video('instalacao-windows', 'Instalação Windows', 251497668),
-]
-
-videos_dct = {video.slug: video for video in videos}
+from django.shortcuts import render, get_object_or_404
+from pypro.aperitivos.models import Video
 
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    video = videos_dct[slug]
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
