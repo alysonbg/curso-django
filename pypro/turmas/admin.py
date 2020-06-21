4262032d5b94c9f103dfba524a3c7914a35f36c2
Alyson Girao <alysongirao@gmail.com>
2020-06-20 23:26:26 -0300

    Criado app de turmas

---
+++ pypro/turmas/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from pypro.turmas.models import Turma
+
+
+class MatriculaInline(admin.TabularInline):
+    model = Turma.alunos.through
+    extra = 1
+    readonly_fields = ('data',)
+    autocomplete_fields = ('usuario',)
+    ordering = ('data',)
+
+
+@admin.register(Turma)
+class TurmasAdmin(admin.ModelAdmin):
+    inlines = [MatriculaInline]
+    list_display = ('nome', 'slug', 'inicio', 'fim')
+    prepopulated_fields = {'slug': ('nome',)}
+    ordering = ('-inicio',)
